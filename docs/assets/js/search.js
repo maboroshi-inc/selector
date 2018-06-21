87c622b71376d2c9438314439da3a6f4a6fc2d97
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-06-21 18:14:23 +0900

    ドキュメントを更新した。

---
+++ docs/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"$$","url":"globals.html#__","classes":"tsd-kind-function tsd-has-type-parameter"},{"id":1,"kind":64,"name":"$","url":"globals.html#_","classes":"tsd-kind-function tsd-has-type-parameter"}]};
