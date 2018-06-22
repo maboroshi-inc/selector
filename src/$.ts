72c61affaf922f91b17384622288d25183ecfa84
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-06-22 11:50:21 +0900

    `$()` の型定義を修正した。

--- src/$.ts
+++ src/$.ts
@@ -5,9 +5,24 @@
  * @param ctx コンテキスト要素
  * @returns `ctx` から `selector` にマッチする最初の要素を返す
  */
+export function $<K extends keyof HTMLElementTagNameMap>(
+  selector: K,
+  ctx?: ParentNode
+): HTMLElementTagNameMap[K] | null
+
+export function $<K extends keyof SVGElementTagNameMap>(
+  selector: K,
+  ctx?: ParentNode
+): SVGElementTagNameMap[K] | null
+
 export function $<E extends Element = Element>(
   selector: string,
+  ctx?: ParentNode
+): E | null
+
+export function $(
+  selector: keyof HTMLElementTagNameMap | keyof SVGElementTagNameMap | string,
   ctx: ParentNode = document
-): E | null {
-  return ctx.querySelector<E>(selector)
+) {
+  return ctx.querySelector(selector)
 }
