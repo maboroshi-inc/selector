e2aaeda4e8cda6257c617cad9c3c7e85c94bd08e
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-10-29 20:25:48 +0900

    `$$()` 関数に Selector 型と MatchedElement 型を引き当てた。

--- src/$$.ts
+++ src/$$.ts
@@ -1,3 +1,5 @@
+import { MatchedElement, Selector } from './types'
+
 /**
  * セレクターにマッチする全ての要素を返す
  *
@@ -5,24 +7,9 @@
  * @param ctx コンテキスト要素
  * @returns `ctx` から `selector` にマッチする全ての要素を返す
  */
-export function $$<K extends keyof HTMLElementTagNameMap>(
-  selector: K,
-  ctx?: ParentNode
-): NodeListOf<HTMLElementTagNameMap[K]>
-
-export function $$<K extends keyof SVGElementTagNameMap>(
-  selector: K,
-  ctx?: ParentNode
-): NodeListOf<SVGElementTagNameMap[K]>
-
-export function $$<E extends Element = Element>(
-  selector: string,
-  ctx?: ParentNode
-): NodeListOf<E>
-
-export function $$(
-  selector: keyof HTMLElementTagNameMap | keyof SVGElementTagNameMap | string,
+export function $$<E extends Element = Element, T extends Selector = string>(
+  selector: T,
   ctx: ParentNode = document
-) {
+): NodeListOf<MatchedElement<T, E>> {
   return ctx.querySelectorAll(selector)
 }
