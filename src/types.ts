11e004299742b5586d014fa936050e59b1ea9a40
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-10-29 20:20:06 +0900

    MatchedElement 型を定義した。

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,16 @@
 /**
+ * 適合要素型
+ */
+export type MatchedElement<
+  T extends Selector,
+  E extends Element
+> = T extends keyof HTMLElementTagNameMap
+  ? HTMLElementTagNameMap[T]
+  : T extends keyof SVGElementTagNameMap
+  ? SVGElementTagNameMap[T]
+  : E
+
+/**
  * セレクター型
  */
 export type Selector =
